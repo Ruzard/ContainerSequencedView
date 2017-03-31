862d6561fee6429ed9bd652470cabb5cdfde0ac1
Konstantin Malyshev <kmalyshev@malwarebytes.com>
2017-03-31 13:21:09 +0300

    Initial Commit

---
+++ SampleContainerSequencedView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SampleContainerSequencedView
+//
+//  Created by Konstantin Malyshev on 3/31/17.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SampleContainerSequencedView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleContainerSequencedView
+//
+//  Created by Konstantin Malyshev on 3/31/17.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
